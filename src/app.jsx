0ae418b0b370523ba5de951802c0c50ee2514a5a
ewgenius <ewgeniux@gmail.com>
2016-06-09 16:39:44 +0300

    upd

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, {Component, PropTypes} from 'react'
 import {render} from 'react-dom'
-import App from './components/App/App.jsx'
+import {createStore} from 'redux'
+import {connect, Provider} from 'react-redux'
 import './styles/main.scss'
 
-async function test() {
-  await Promise.resolve(1)
+const counter = (state = 0, action) => do {
+  if (action.type === 'INCREMENT')
+    state + 1
+  else if (action.type === 'DECREMENT')
+    state - 1
+  else
+    state
 }
 
-console.log(test())
+const store = createStore(counter)
 
-render(<App />, document.getElementById('app'))
+store.subscribe(() => console.log(store.getState()))
+
+class Counter extends Component {
+  static propTypes = {
+    value: PropTypes.number.isRequired,
+    onIncrement: PropTypes.func.isRequired,
+    onDecrement: PropTypes.func.isRequired
+  }
+
+  render() {
+    return <div>
+      {this.props.value}
+      <button onClick={this.props.onIncrement}>+</button>
+      <button onClick={this.props.onDecrement}>-</button>
+    </div>
+  }
+}
+
+render(
+  <Counter
+    value={store.getState()}
+    onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+    onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+  />, document.getElementById('app'))
